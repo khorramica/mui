ef7de8c87c574f1c4c04ef2466b9b13c12830ce9
khorramica <84560576+khorramica@users.noreply.github.com>
2021-12-18 15:33:33 +0330

    theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import { theme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App/>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
