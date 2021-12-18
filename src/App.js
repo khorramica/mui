25d1452dff26111c977f86a29e449d1698431eac
khorramica <84560576+khorramica@users.noreply.github.com>
2021-12-18 13:56:26 +0330

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, makeStyles} from "@material-ui/core";
+import { red } from "@material-ui/core/colors";
+import {  Person } from "@material-ui/icons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles(theme => ({
+  button:{
+    backgroundColor:theme.palette.primary.dark,
+    color:theme.palette.success.light
+  }
+}
+));
+
+const App =()=> {
+  const classes = useStyles();
+  return <div>
+    <Button variant="outlined" size="large" startIcon={<Person/>} color="primary"
+    className={classes.button}
+    >سلام دنیا</Button>
+  </div>
 }
 
 export default App;
